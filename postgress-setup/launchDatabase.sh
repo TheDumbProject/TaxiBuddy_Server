3937fd6a75cacf7fb0095c91082eb0f8bcb854a8
Abhinav-VS21 <abhinav23bcs181@iiitkottayam.ac.in>
2024-08-29 14:20:13 +0530

    added a line to make the script executable for you

--- postgress-setup/launchDatabase.sh
+++ postgress-setup/launchDatabase.sh
@@ -3,6 +3,11 @@
 # Creates the schema for taxiBuddy
 psql -U postgres -a -f ~/TaxiBuddy_Server/postgress-setup/TaxiBuddyDatabase.sql
 
+
+# Makes it executable
+chmod +x ~/TaxiBuddy_Server/postgress-setup/TaxiBuddyData.sh
+
+
 # populates the database
 ~/TaxiBuddy_Server/postgress-setup/TaxiBuddyData.sh
 
