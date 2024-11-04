0676a4b9d5398eb5e88b452c3928916923f1dc2a
MrShah_21 <abhinav23bcs181@iiitkottayam.ac.in>
2024-11-04 11:27:06 +0530

    Update launchDatabase.sh
    this correctly launch the database with data

--- postgress-setup/launchDatabase.sh
+++ postgress-setup/launchDatabase.sh
@@ -2,13 +2,3 @@
 
 # Creates the schema for taxiBuddy
 psql -U postgres -a -f ~/TaxiBuddy_Server/postgress-setup/TaxiBuddyDatabase.sql
-
-
-# Makes it executable
-chmod +x ~/TaxiBuddy_Server/postgress-setup/TaxiBuddyData.sh
-
-
-# populates the database
-~/TaxiBuddy_Server/postgress-setup/TaxiBuddyData.sh
-
-psql -U postgres -d taxi_buddy -c "\l" -c "\dt"
